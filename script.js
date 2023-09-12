6af1258910de6763eaf0d567d17640c5734cda49
Imael Rennan <imael.rennan@ufms.br>
2023-09-12 11:08:30 -0400

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement // pegando a tag html
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Perfil para modo light")
+  } else {
+    img.setAttribute("src", "./assets/avatar-dark.png")
+    img.setAttribute("alt", "Perfil para modo dark")
+  }
+}
